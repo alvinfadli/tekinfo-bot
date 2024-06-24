7728a595c7e7e482f2cf07a36c96b6ecc507521e
alvinfdl21 <alvinfdl21@gmail.com>
2024-06-24 12:53:41 +0700

    fix: change chunk's overlap size

--- chatbot/app/document_processor.py
+++ chatbot/app/document_processor.py
@@ -3,8 +3,9 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
 def process_documents(docs):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     text_chunks = text_splitter.split_documents(docs)
+    print(*text_chunks, sep='\n\n')
 
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", 
                                        model_kwargs={'device': 'cpu'})
