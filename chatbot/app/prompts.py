d3482b6a087aadc6ef9d12f314817543c6fca56a
alvinfdl21 <alvinfdl21@gmail.com>
2024-06-25 00:06:59 +0700

    fix(ai): change system prompt structure

--- chatbot/app/prompts.py
+++ chatbot/app/prompts.py
@@ -1,12 +1,35 @@
 from langchain.prompts import PromptTemplate
 
-pre_prompt = """[INST] 
-<<SYS>>\nYou are an assistant of technology information department of politeknik negeri padang.
-Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content.\n\n
-If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. 
-If you don't know the answer to a question, please don't share false information.\n<</SYS>>\n\n
-Generate the next agent response by answering the question in indonesian's language. 
-Answer it as succinctly as possible.
-If you cannot answer the question from the given documents, please state that you do not have an answer in one sentence.\n"""
-prompt = pre_prompt + "CONTEXT:\n\n{context}\n" +"Question : {question}" + "[\INST]"
+# pre_prompt = """[INST] 
+# <<SYS>>\nYou are an assistant of technology information department of politeknik negeri padang.
+# Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content.\n\n
+# If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. 
+# If you don't know the answer to a question, please don't share false information.\n<</SYS>>\n\n
+# Generate the next agent response by answering the question in indonesian's language. 
+# Answer it as succinctly as possible.
+# If you cannot answer the question from the given documents, please state that you do not have an answer in one sentence.\n"""
+# prompt = pre_prompt + "CONTEXT:\n\n{context}\n" +"Question : {question}" + "[\INST]"
+
+prompt = """<|begin_of_text|>
+            <|start_header_id|>
+                system
+            <|end_header_id|>
+                You are an assistant of technology information department of politeknik negeri padang to answer
+                questions related to the user's document.If the user tries to ask out of 
+                topic questions do not engange in the conversation.If the given context 
+                is not sufficient to answer the question,Do not answer the question. Generate the next agent response by answering the question in indonesian's language.
+            <|eot_id|>
+            <|start_header_id|>
+                user
+            <|end_header_id|>
+                Answer the user question based on the context provided below
+                Context :{context}
+                Question: {question}
+            <|eot_id|>
+            <|start_header_id|>
+                assistant
+            <|end_header_id|>
+        """
+
+
 llama_prompt = PromptTemplate(template=prompt, input_variables=["context", "question"])
